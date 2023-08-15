4f57d84e968445ff27ad3b899a3fdf0a6cfd0e19
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2023-08-16 07:57:20 +0800

    Added in tree

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,72 @@
 import React from 'react';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
+import { Box, Stack, Typography } from '@mui/material';
 
+
+interface RenderTree {
+    id: string;
+    name: string;
+    children?: [{
+        id: string;
+        name: string;
+    }]
+}
+
+const data: RenderTree[] = [{
+    id: 'root',
+    name: 'Parent',
+    children: [
+        {
+            id: '1',
+            name: 'Child - 1',
+        }
+    ],
+},
+{
+    id: 'root2',
+    name: 'Parent2',
+    children: [
+        {
+            id: '2',
+            name: 'Child - 2',
+        }
+    ],
+}];
 
 const Home: React.FunctionComponent = () => {
     document.title = "Home";
 
+    const renderTree = (nodes: RenderTree) => (
+
+
+        <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+            {Array.isArray(nodes.children)
+                ? nodes.children.map((node) => renderTree(node))
+                : null}
+        </TreeItem>
+    );
+
 
     return (
         <>
-            <h2 style={{ textAlign: "center" }}>Test Data</h2>
-
+            <Box>
+                <Typography variant='h5' color="secondary" align='center'>
+                    Example Data
+                </Typography>
+                <Stack spacing={3} direction="row">
+                    <TreeView
+                        aria-label="rich object"
+                        defaultCollapseIcon={<ExpandMoreIcon />}
+                        defaultExpanded={['root']}
+                        defaultExpandIcon={<ChevronRightIcon />}
+                    >
+                        {data.map((node) => renderTree(node))}
+                    </TreeView>
+                </Stack>
+            </Box>
         </>
     );
 };
